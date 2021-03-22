5b60a557cbf70c3ef08024800a1c9c2ac00d0d7d
Sadia Zaman <sadia.sorno.zaman@gmail.com>
2021-03-22 14:23:48 +0600

    lab1 done

---
+++ lab1/dashboard.php
@@ -0,0 +1,14 @@
+<?php 
+		session_start();
+			if(!isset ($_COOKIE["user"])){
+				header("Location : login.php");
+			}
+?>
+<html> 
+		<body> 
+				 
+			<h1> Welcome<?php echo $_COOKIE["user"];?></h1>
+		    <a href = "change_password.php"> Change Password</a>
+		</body>
+
+</html>
